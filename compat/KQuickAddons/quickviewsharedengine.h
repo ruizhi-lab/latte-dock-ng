013eb3cc7aadb6495719108b3f613737161e7649
Ruizhi Zhong <ruizhi.zhong@outlook.com>
2026-04-29 00:17:34 +0800

    Fix KF6 Package linkage and QuickViewSharedEngine compat include

--- compat/KQuickAddons/quickviewsharedengine.h
+++ compat/KQuickAddons/quickviewsharedengine.h
@@ -1,3 +1,13 @@
 #pragma once
 
-#include <KQuickAddons/quickviewsharedengine.h>
+#if __has_include(<KQuickAddons/QuickViewSharedEngine>)
+#include <KQuickAddons/QuickViewSharedEngine>
+#elif __has_include(<kquickaddons/quickviewsharedengine.h>)
+#include <kquickaddons/quickviewsharedengine.h>
+#elif __has_include(<plasmaquick/quickviewsharedengine.h>)
+#include <plasmaquick/quickviewsharedengine.h>
+#elif __has_include_next(<KQuickAddons/quickviewsharedengine.h>)
+#include_next <KQuickAddons/quickviewsharedengine.h>
+#else
+#error "Could not find a usable QuickViewSharedEngine header"
+#endif
